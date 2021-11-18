08100e1248b74c5f256b9f4dfe1d27db54fbafd9
myunghoon96 <69111087+myunghoon96@users.noreply.github.com>
2021-11-18 15:51:01 +0900

    commit

--- springboot_jpa_myshop/my_shop/src/main/java/com/my_shop/constant/Role.java
+++
@@ -1,5 +0,0 @@
-package com.my_shop.constant;
-
-public enum Role {
-    USER, ADMIN
-}
--- springboot_jpa_myshop/my_shop/src/main/java/com/my_shop/repository/CartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.my_shop.repository;
-
-import com.my_shop.entity.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-
-}
