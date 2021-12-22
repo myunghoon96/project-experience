fab04999d55f6309399bf97b640da02a852c1a5d
myunghoon96 <qkdlqkdl44@gmail.com>
2021-12-22 17:15:07 +0900

    initiallize

---
+++ springboot_jpa_mall/src/main/java/com/springboot_jpa_mall/SpringbootJpaMallApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_jpa_mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaMallApplication.class, args);
+	}
+
+}
