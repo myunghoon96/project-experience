b080ec29cbf1457ecdce750fcce39ea7fddf2c6f
myunghoon96 <qkdlqkdl44@gmail.com>
2021-12-28 14:19:39 +0900

    Merge branch 'main' of https://github.com/myunghoon96/project-experience

--- springboot_jpa_myshop/my_shop/src/main/java/com/my_shop/repository/CartItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.my_shop.repository;
-
-import com.my_shop.entity.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-    CartItem findByCartIdAndItemId(Long cartId, Long itemId);
-}
