618699385f6fc610a89ea4c9bd8467399965cadd
myunghoon96 <qkdlqkdl44@gmail.com>
2021-12-28 14:05:43 +0900

    Update: Displaying Image
    Displaying Image

--- springboot_jpa_mall/src/main/java/com/springboot_jpa_mall/repository/ImageRepository.java
+++ springboot_jpa_mall/src/main/java/com/springboot_jpa_mall/repository/ImageRepository.java
@@ -4,12 +4,13 @@ import com.springboot_jpa_mall.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
 
-    Optional<Image> findById(Long id);
+    List<Image> findAllByItemId(Long id);
     Optional<Image> findTop1ByItemId(Long itemId);
 
 }
--- springboot_jpa_mall/src/main/java/com/springboot_jpa_mall/service/ImageService.java
+++ springboot_jpa_mall/src/main/java/com/springboot_jpa_mall/service/ImageService.java
@@ -2,11 +2,16 @@ package com.springboot_jpa_mall.service;
 
 import com.springboot_jpa_mall.entity.Image;
 import com.springboot_jpa_mall.repository.ImageRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.thymeleaf.util.StringUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+@Slf4j
 @Service
 public class ImageService {
     @Autowired
@@ -21,7 +26,9 @@ public class ImageService {
 
         //파일 업로드
         if(!StringUtils.isEmpty(originalFilename)){
-            imageName = fileService.uploadFile("C:/Users/NKNK-DESKTOP/Desktop/project-experience/springboot_jpa_mall/src/main/resources/static/images/", originalFilename,
+//            imageName = fileService.uploadFile("C:/Users/NKNK-DESKTOP/Desktop/project-experience/springboot_jpa_mall/src/main/resources/static/images/", originalFilename,
+//                    itemImgFile.getBytes());
+            imageName = fileService.uploadFile("/Users/nknk/Desktop/project-experience/springboot_jpa_mall/src/main/resources/static/images/", originalFilename,
                     itemImgFile.getBytes());
             imageUrl = "/images/" + imageName;
         }
@@ -33,6 +40,26 @@ public class ImageService {
         imageRepository.save(itemImg);
 
         return itemImg;
+    }
+
+    public Image findImageByItemId(Long itemId) {
+        Optional<Image> imageWrapper = imageRepository.findTop1ByItemId(itemId);
+        Image image = imageWrapper.get();
+        return image;
+    }
+
+    public List<String> findImageUrlsByItemId(Long itemId) {
+        List<Image> images = imageRepository.findAllByItemId(itemId);
+        List<String> imageUrls = new ArrayList<>();
+
+        log.info("findImageUrlsByItemId");
+
+        for (Image image : images) {
+            imageUrls.add(image.getImageUrl());
+            log.info("{}", image.getImageUrl());
+        }
+
+        return imageUrls;
     }
 
 }
